9373a9ae6fb9b26b15945d2230c8e03031d727c8
vamshi-8688 <vamshikallem12345@gmail.com>
2026-09-11 16:49:35 +0530

    app added

--- test.py
+++ test.py
@@ -1,9 +1,18 @@
 from tools.tavily_tool import tavily_search 
 from tools.flight_tool import search_flights
-
+from backend import run_travel_agent
 
 #res = tavily_search("Best hotels in india")
 # print(res)
 
-res = search_flights("Plan a 7 days Nepal trip from India")
-print(res)
+#res = search_flights("Plan a 7 days Nepal trip from India")
+#print(res)
+
+user_input = input("Enter travel requesst")
+
+response = run_travel_agent(
+    user_input= user_input,
+    thread_id= "test_user"
+)
+
+print(response["answer"])
